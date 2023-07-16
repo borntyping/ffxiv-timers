5ae9fe865fe50cc12c802676972ecf47e2768e14
Sam Clements <sam@borntyping.co.uk>
2023-07-16 18:27:03 +0100

    Add a footer

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,22 @@
   </div>
 </main>
 
-<style>
+<footer class="card">
+  <p>Created by <a href="https://hachyderm.io/@borntyping">@borntyping</a>. Source at <a href="https://github.com/borntyping/ffxiv-timers">github.com/borntyping/ffxiv-timers</a>.</p>
+  <p>Directly inspired by <a href="https://www.xenoveritas.org/static/ffxiv/timer.html">Xenoveritas's timer</a>.</p>
+  <p>FINAL FANTASY XIV ©2010-2023 SQUARE ENIX CO., LTD.</p>
+</footer>
 
+<style>
+    footer {
+        color: #999999;
+    }
+
+    footer a {
+        color: #666666;
+    }
+
+    footer p {
+        margin: 0;
+    }
 </style>
